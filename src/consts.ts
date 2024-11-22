5f2df1e2c790e39290efa513f2b2f5f491441b1e
MrJuneJune <parkjune1995@gmail.com>
2024-11-22 02:50:27 -0500

    Added blogs related to blue noises.

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "MrJuneJune";
-export const SITE_DESCRIPTION = "Sup";
+export const SITE_DESCRIPTION = "Resume / Blogging website";
